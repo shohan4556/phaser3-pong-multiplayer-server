1a1259d4c899f1cc58950963dc7a01ef1261a99c
Shohanur Rahaman <shohan4556@gmail.com>
2023-02-15 12:59:25 +0600

    updated with glitch works fine

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-        origin: [`http://localhost:${process.env.CLIENT_PORT}`],
+        origin: '*',
+        // origin: [`http://localhost:${process.env.CLIENT_PORT}`],
         methods: ['GET', 'POST'],
-        credentials: true,
+        // credentials: true,
     },
 });
 
@@ -29,21 +30,22 @@ app.get('/', (req, res) => {
 let readyPlayerCount = 0;
 let players = {}; // keep track of players
 const roomName = 'myroom_1';
-const gameNameSpace = io.of('/play');
+// const gameNameSpace = io.of('/play');
 
-gameNameSpace.on('connection', (socket) => {
+io.on('connection', (socket) => {
+    console.log('connected ', socket.id);
     players[socket.id] = { id: socket.id, isRefree: false };
     // join to the room
     socket.join(roomName);
 
-    socket.on('ready', (data) => {
-        // console.log('+++++ a player is ready', data);
+    socket.on('ready', () => {
+        console.log('+++++ a player is ready');
         readyPlayerCount += 1;
         // console.log('ready player count', readyPlayerCount);
         if (readyPlayerCount === 2) {
             // brodcast to all players/clients
             players[socket.id] = { id: socket.id, isRefree: true };
-            gameNameSpace.in(roomName).emit('startGame', players);
+            io.in(roomName).emit('startGame', players);
             // console.log('+++++ start game +++++', players);
         }
     }); // end function
@@ -64,7 +66,7 @@ gameNameSpace.on('connection', (socket) => {
         if (readyPlayerCount <= 0) readyPlayerCount = 0;
         if (players[socket.id]) delete players[socket.id];
 
-        gameNameSpace.in(roomName).emit('playerLeft', { id: socket.id });
+        io.in(roomName).emit('playerLeft', { id: socket.id });
         // leave from the room
         socket.leave(roomName);
         console.log('----- disconnect', players);
@@ -72,5 +74,6 @@ gameNameSpace.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
+    console.log('io ', io);
     console.log(`server is running on port http://localhost:${PORT}`);
 });
